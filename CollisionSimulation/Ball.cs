2b11dc9d1c2578fe26e917dd67819be5a2682223
thefoggycity <thefoggycity@live.cn>
2016-03-23 23:06:26 +0800

    Initial upload.

---
+++ CollisionSimulation/Ball.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollisionSimulation
+{
+    class Ball
+    {
+        public Double Mass, R;
+        public struct Position
+        {
+            public Double x;
+            public Double y;
+            public Position(Double X, Double Y)
+            {
+                x = X;
+                y = Y;
+            }
+        }
+        public Position Pos;
+        public struct Momentum
+        {
+            public Double x;
+            public Double y;
+            public Momentum(Double X, Double Y)
+            {
+                x = X;
+                y = Y;
+            }
+        }
+        public Momentum P;
+        public struct Velocity
+        {
+            public Double x;
+            public Double y;
+            public Velocity(Double X, Double Y)
+            {
+                x = X;
+                y = Y;
+            }
+        }
+
+
+        public Ball(Double BallMass, Double BallRadius, Position BallPosition, Momentum BallMomentum)
+        {
+            Mass = BallMass;
+            R = BallRadius;
+            Pos = BallPosition;
+            P = BallMomentum;
+        }
+        public Ball(Double BallMass, Double BallRadius, Position BallPosition, Velocity BallVelocity)
+        {
+            Mass = BallMass;
+            R = BallRadius;
+            Pos = BallPosition;
+            P = Calc.GetMomentumByVelocity(Mass, BallVelocity);
+        }
+        public Ball(Double BallMass, Double BallRadius, Double PosX, Double PosY, Double MomentumX, Double MomentumY)
+        {
+            Position BallPosition = new Position(PosX, PosY);
+            Momentum BallMomentum = new Momentum(MomentumX, MomentumY);
+            Mass = BallMass;
+            R = BallRadius;
+            Pos = BallPosition;
+            P = BallMomentum;
+        }
+        public Ball(Double BallMass, Double BallRadius, Double PosX, Double PosY)
+        {
+            Position BallPosition = new Position(PosX, PosY);
+            Momentum BallMomentum = new Momentum(0, 0);
+            Mass = BallMass;
+            R = BallRadius;
+            Pos = BallPosition;
+            P = BallMomentum;
+        }
+        public Ball(Ball BallToCopy)
+        {
+            Mass = BallToCopy.Mass;
+            R = BallToCopy.R;
+            Pos = BallToCopy.Pos;
+            P = BallToCopy.P;
+        }
+
+        public void SetValue(Ball BallToCopy)
+        {
+            Mass = BallToCopy.Mass;
+            R = BallToCopy.R;
+            Pos = BallToCopy.Pos;
+            P = BallToCopy.P;
+        }
+        public void SetMomentum(Double MomentumX, Double MomentumY)
+        {
+            P.x = MomentumX;
+            P.y = MomentumY;
+        }
+        public void SetMomentumByVelocity(Double Vx, Double Vy)
+        {
+            P.x = Vx * Mass;
+            P.y = Vy * Mass;
+        }
+        public void SetMomentumByVelocity(Velocity BallVelocity)
+        {
+            P = Calc.GetMomentumByVelocity(this.Mass, BallVelocity);
+        }
+        public void SetPosition(Double BallPosX, Double BallPosY)
+        {
+            Pos.x = BallPosX;
+            Pos.y = BallPosY;
+        }
+        public void Move(Double TimeRate)
+        {
+            Pos.x += (GetVx() * TimeRate);
+            Pos.y += (GetVy() * TimeRate);
+        }
+
+        public Momentum CollideWith(ref Ball TargetBall, Double TimeRate)
+        {
+            Ball tmp = new Ball(this);
+            Momentum imp = Calc.Collide(ref tmp, ref TargetBall,TimeRate );
+            SetValue(tmp);
+            return imp;
+        }
+        public Momentum CollideWith(ref Ball TargetBall)
+        {
+            return CollideWith(ref TargetBall, 0);
+        }
+
+
+        public Double GetResultantMomentum()
+        {
+            return Calc.GetResultantMomentum(this);
+        }
+        public Double GetResultantVelocity()
+        {
+            return Calc.GetResultantVelocity(this);
+        }
+        public Velocity GetVelocity()
+        {
+            return new Velocity(this.GetVx(), this.GetVy());
+        }
+        public Double GetVx()
+        {
+            return P.x / Mass;
+        }
+        public Double GetVy()
+        {
+            return P.y / Mass;
+        }
+        public Double GetDistance(Ball TargetBall)
+        {
+            return Calc.GetDistance(this, TargetBall);
+        }
+        public Double GetDistance(Position TargetPosition)
+        {
+            return Calc.GetDistance(this.Pos, TargetPosition);
+        }
+        public Double GetDistance(Double PosX, Double PosY)
+        {
+            return Calc.GetDistance(this.Pos, new Position(PosX, PosY));
+        }
+        public Double ChkCollision(Ball TargetBall)
+        {
+            return Calc.ChkCollision(this, TargetBall);
+        }
+        
+        public static class Calc
+        {            
+            public static Double GetDistance(Position Ball1Pos, Position Ball2Pos)
+            {
+                return Math.Sqrt(Math.Pow(Ball1Pos.x - Ball2Pos.x, 2) + Math.Pow(Ball1Pos.y - Ball2Pos.y, 2));
+            }
+            public static Double GetDistance(Ball Ball1, Ball Ball2)
+            {
+                return GetDistance(Ball1.Pos, Ball2.Pos);
+            }
+
+            public static Double GetResultantMomentum(Momentum BallP)
+            {
+                return Vector.Mode(BallP);
+            }
+            public static Double GetResultantMomentum(Ball TargetBall)
+            {
+                return Vector.Mode(TargetBall.P);
+            }
+
+            public static Momentum GetMomentumByVelocity(Double BallMass, Double Vx, Double Vy)
+            {
+                return new Momentum(BallMass * Vx, BallMass * Vy);
+            }
+            public static Momentum GetMomentumByVelocity(Double BallMass, Velocity BallVelocity)
+            {
+                return new Momentum(BallMass * BallVelocity.x, BallMass * BallVelocity.y);
+            }
+
+            public static Double GetResultantVelocity(Ball TargetBall)
+            {
+                return Vector.Mode(TargetBall.P) / TargetBall.Mass;
+            }
+
+            public static Velocity GetVelocity(Ball TargetBall)
+            {
+                return new Velocity(TargetBall.P.x / TargetBall.Mass, TargetBall.P.y / TargetBall.Mass);
+            }
+            public static Velocity GetVelocity(Double BallMass, Momentum BallP)
+            {
+                return new Velocity(BallP.x / BallMass, BallP.y / BallMass);
+            }
+
+            public static Double ChkCollision(Ball Ball1, Ball Ball2)
+            {
+                return (GetDistance(Ball1, Ball2) - Ball1.R - Ball2.R);
+            }
+
+            public static Single CorrectPosition(ref Ball Ball1, ref Ball Ball2, Double TimeRate)
+            {
+                const UInt16 ACCURACY = 20;
+                UInt16 i;
+                Double d = ChkCollision(Ball1, Ball2);
+                if (d > 0)
+                    return -1;
+                Velocity v1 = new Velocity
+                {
+                    x = -TimeRate * Ball1.GetVx() / ACCURACY,
+                    y = -TimeRate * Ball1.GetVy() / ACCURACY,
+                };
+                Velocity v2 = new Velocity
+                {
+                    x = -TimeRate * Ball2.GetVx() / ACCURACY,
+                    y = -TimeRate * Ball2.GetVy() / ACCURACY,
+                };
+                for (i = 0; i < ACCURACY; i++)
+                {
+                    Ball1.Pos = Vector.Add(Ball1.Pos, v1);
+                    Ball2.Pos = Vector.Add(Ball2.Pos, v2);
+                    if (ChkCollision(Ball1, Ball2) >= 0)
+                    {
+                        Ball1.Pos = Vector.Add(Ball1.Pos, Vector.Multiple(v1, -1));
+                        Ball2.Pos = Vector.Add(Ball2.Pos, Vector.Multiple(v2, -1));
+                        return (Single)(i * 1.0 / ACCURACY);
+                    }
+                }
+                return -1;
+            }
+
+            public static Momentum Collide(ref Ball Ball1, ref Ball Ball2, Double TimeRate)
+            {
+                Double d = ChkCollision(Ball1, Ball2);
+                Single Correction = 0;
+                if (d > 0)
+                    return new Momentum(0, 0);
+                if (TimeRate != 0)
+                    Correction = CorrectPosition(ref Ball1, ref Ball2, TimeRate);
+                Momentum Imp = new Momentum(Ball2.Pos.x - Ball1.Pos.x, Ball2.Pos.y - Ball1.Pos.y);
+                if (Imp.x != 0)
+                {
+                    Double k = Imp.y / Imp.x;           //j=ki
+                    Imp.x = 2 * (Ball1.P.x * Ball2.Mass - Ball2.P.x * Ball1.Mass + k * Ball1.P.y * Ball2.Mass - k * Ball2.P.y * Ball1.Mass)
+                        / ((Ball1.Mass + Ball2.Mass) * (k * k + 1));
+                    Imp.y = k * Imp.x;
+                }
+                else
+                {
+                    Imp.y = 2 * (Ball2.Mass * Ball1.P.y - Ball1.Mass * Ball2.P.y) / (Ball1.Mass + Ball2.Mass);
+                }
+                Ball1.P = Vector.Add(Ball1.P, Vector.Multiple(Imp, -1));
+                Ball2.P = Vector.Add(Ball2.P, Imp);
+                if (TimeRate != 0 && Correction > 0)
+                {
+                    Ball1.Move(TimeRate * Correction);
+                    Ball2.Move(TimeRate * Correction);
+                }
+                return Imp;
+            }
+            public static Momentum Collide(ref Ball Ball1, ref Ball Ball2)
+            {
+                return Collide(ref Ball1, ref Ball2, 0);
+            }
+
+            static class Vector
+            {
+                public static Momentum Multiple(Momentum p, Double Multipler)
+                {
+                    p.x *= Multipler;
+                    p.y *= Multipler;
+                    return p;
+                }
+                public static Velocity Multiple(Velocity v, Double Multipler)
+                {
+                    v.x *= Multipler;
+                    v.y *= Multipler;
+                    return v;
+                }
+                public static Momentum Add(Momentum p1, Momentum p2)
+                {
+                    return new Momentum(p1.x + p2.x, p1.y + p2.y);
+                }
+                public static Position Add(Position  pos, Velocity v)
+                {
+                    return new Position(pos.x + v.x, pos.y + v.y);
+                }
+                public static Double Mode(Momentum p)
+                {
+                    return Math.Sqrt(Math.Pow(p.x, 2) + Math.Pow(p.y, 2));
+                }
+                //public static Momentum GetMeta(Momentum p)
+                //{
+                //    Double l = Mode(p);
+                //    return new Momentum(p.x / l, p.y / l);
+                //}
+            }
+        }
+    }
+}
---
+++ CollisionSimulation/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollisionSimulation
+{
+    public partial class Form1 : Form
+    {
+        VisibleBall a, b;
+        Double tr;
+        Ball.Position O;
+        Bitmap bmp;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            Ball.Momentum m = Ball.Calc.Collide(ref a, ref b);
+            MessageBox.Show(a.GetVx().ToString() + " " + a.GetVy().ToString() + "\n"
+                + b.GetVx().ToString() + " " + b.GetVy().ToString() + "\n"
+                + m.x.ToString() + " " + m.y.ToString());
+             */
+            a = new VisibleBall(0.5, 14.15, Color.MediumVioletRed, 0, 60, 15, -7);
+            b = new VisibleBall(1, 14.15, Color.Lime, 80, 0, -12, 5);
+            O.x = 200;
+            O.y = 100;
+            tr = 0.2;
+
+            this.BackgroundImageLayout = ImageLayout.Center;
+            this.BackgroundImage = bmp;
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a.Move(tr);
+            b.Move(tr);
+            if (VisibleBall.ChkCollision(a, b) <= 0)
+                VisibleBall.Collide(ref a, ref b, tr);
+
+            bmp = new Bitmap(this.Width, this.Height);
+            g = Graphics.FromImage(bmp);
+            a.Draw(ref g, O);
+            b.Draw(ref g, O);
+
+            this.BackgroundImage = bmp;
+            this.Refresh();
+        }
+
+        sealed class VisibleBall : Ball
+        {
+            Color Col;
+
+            public VisibleBall(Double BallMass, Double BallRadius, Color BallColor, Position BallPosition, Momentum BallMomentum)
+                : base (BallMass, BallRadius, BallPosition, BallMomentum)
+            {
+                Col = BallColor;
+            }
+            public VisibleBall(Double BallMass, Double BallRadius, Color BallColor, Position BallPosition, Velocity BallVelocity)
+                : base(BallMass, BallRadius, BallPosition, Calc.GetMomentumByVelocity(BallMass, BallVelocity))
+            {
+                Col = BallColor;
+            }
+            public VisibleBall(Double BallMass, Double BallRadius, Color BallColor, Double X, Double Y, Double Vx, Double Vy)
+                : base(BallMass, BallRadius, X, Y, BallMass * Vx, BallMass * Vy)
+            {
+                Col = BallColor;
+            }
+            public VisibleBall (Ball OriginalBall, Color BallColor)
+                : base(OriginalBall)
+            {
+                Col=BallColor;
+            }
+
+            public void Draw(ref Graphics Gra, Double ShiftX, Double ShiftY)
+            {
+                SolidBrush brsh = new SolidBrush(Col);
+                Gra.FillEllipse(brsh, (float)(Pos.x - R + ShiftX), (float)(Pos.y - R + ShiftY), (float)(2 * R), (float)(2 * R));
+                brsh.Dispose();
+            }
+            public void Draw(ref Graphics Gra, Position OriginPoint)
+            {
+                Draw(ref Gra, OriginPoint.x, OriginPoint.y);
+            }
+            public void Draw(ref Graphics Gra)
+            {
+                Draw(ref Gra, 0, 0);
+            }
+
+            public Ball Convert()
+            {
+                return ConvertType(this);
+            }
+
+            public static Ball ConvertType(VisibleBall TargetBall)
+            {
+                return new Ball(TargetBall.Mass, TargetBall.R, TargetBall.Pos, TargetBall.P);
+            }
+            public static VisibleBall ConvertType(Ball TargetBall, Color BallColor)
+            {
+                return new VisibleBall(TargetBall, BallColor);
+            }
+
+            public static Momentum Collide(ref VisibleBall Ball1, ref VisibleBall Ball2, Double TimeRate)
+            {
+                Ball tmp1 = Ball1.Convert(),
+                    tmp2 = Ball2.Convert();
+                Momentum imp = Calc.Collide(ref tmp1, ref tmp2, TimeRate);
+                Ball1.SetValue(tmp1);
+                Ball2.SetValue(tmp2);
+                return imp;
+
+            }
+            public static Momentum Collide(ref VisibleBall Ball1, ref VisibleBall Ball2)
+            {
+                Ball tmp1 = Ball1.Convert(),
+                    tmp2 = Ball2.Convert();
+                Momentum imp = Calc.Collide(ref tmp1, ref tmp2);
+                Ball1.SetValue(tmp1);
+                Ball2.SetValue(tmp2);
+                return imp;
+            }
+            public static Double ChkCollision(VisibleBall Ball1, VisibleBall Ball2)
+            {
+                Ball tmp1 = Ball1.Convert(),
+                    tmp2 = Ball2.Convert();
+                return Calc.ChkCollision(tmp1, tmp2);
+            }
+        }
+    }
+}
